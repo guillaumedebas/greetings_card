eb10d4b5535054eb28d32620470460170cf1cff6
Guillaume Debas <guillaumedebas@hotmail.fr>
2023-12-19 23:49:23 +0100

    Navbar icons added

---
+++ src/components/Navbar/consts/navbarItems.jsx
@@ -0,0 +1,47 @@
+import PeopleIcon from '@mui/icons-material/People';
+import StorageIcon from '@mui/icons-material/Storage';
+import ImageIcon from '@mui/icons-material/Image';
+import PublicIcon from '@mui/icons-material/Public';
+import CodeIcon from '@mui/icons-material/Code';
+import SettingsInputCompositeIcon from '@mui/icons-material/SettingsInputComposite';
+
+
+
+
+export const mainNavbarItems = [{
+    id: 0,
+    icon: <PeopleIcon />,
+    label: 'Authentication',
+    route: 'route'
+} ,
+{
+    id: 1,
+    icon: <StorageIcon />,
+    label: 'Database',
+    route: 'route'
+} ,
+{
+    id: 2,
+    icon: <ImageIcon />,
+    label: 'Storage',
+    route: 'route'
+} ,
+{
+    id: 3,
+    icon: <PublicIcon />,
+    label: 'Hosting',
+    route: 'route'
+} ,
+{
+    id: 4,
+    icon: <CodeIcon />,
+    label: 'Functions',
+    route: 'route'
+} ,
+{
+    id: 5,
+    icon: <SettingsInputCompositeIcon />,
+    label: 'Machine learning',
+    route: 'route'
+} ,
+]
