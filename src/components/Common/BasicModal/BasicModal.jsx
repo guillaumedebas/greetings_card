feff10e4a860a0ecc8f46704be7bcc89264735fe
Guillaume Debas <guillaumedebas@hotmail.fr>
2023-12-29 18:02:10 +0100

    modal system update

--- src/components/Common/BasicModal/BasicModal.jsx
+++ src/components/Common/BasicModal/BasicModal.jsx
@@ -1,36 +1,46 @@
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import CommonButton from '../CommonButton/CommonButton';
 import PropTypes from 'prop-types';
+import { modalStyles } from './styles';
 
-const BasicModal = ({open, onClose}) => {
-    const style = {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 400,
-        bgcolor: 'background.paper',
-        border: '2px solid #000',
-        boxShadow: 24,
-        p: 4,
-    };
+
+const BasicModal = ({ open, onClose, title, subTitle, content, onSubmit }) => {
     return (
-        <Modal open={open} onClose={onClose}>
-            <Box sx={style} >
-                <Typography id="modal-modal-title" variant="h6" component="h2">
-                    Text in a modal
+        <Modal open={open} onClose={onClose} >
+            <Box sx={modalStyles.wrapper}>
+                <Typography
+                    variant="h6"
+                    component="h2"
+                >
+                    {title}
                 </Typography>
-                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                    Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                <Typography sx={{ mt: 2 }}>
+                    {subTitle}
                 </Typography>
+                {content}
+                <Box sx={modalStyles.buttons}>
+                    <CommonButton
+                        variant="contained"
+                        onClick={onSubmit}
+                    >
+                        Submit
+                    </CommonButton>
+                    <CommonButton onClick={onClose}>Cancel</CommonButton>
+                </Box>
             </Box>
         </Modal>
     )
 }
 BasicModal.propTypes = {
-    open: PropTypes.bool.isRequired,
-    onClose: PropTypes.func
+  open: PropTypes.bool.isRequired,
+  onClose: PropTypes.func,
+  title: PropTypes.string, 
+  subTitle: PropTypes.string, 
+  content: PropTypes.node, 
+  onSubmit: PropTypes.func 
 }
+
 
 export default BasicModal
--- src/pages/Authentication/Authentication.jsx
+++ src/pages/Authentication/Authentication.jsx
@@ -6,67 +6,96 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import { cardHeaderStyles } from './styles';
-import GripWrapper from '../../components/GridWrapper/GripWrapper';
-import BasicModal from '../../components/Common/BasicModal/BasicModal';
 import { useState } from 'react';
+import GridWrapper from '../../components/GridWrapper/GripWrapper';
+import NewUserModal from '../../components/Modals/NewUserModal/NewUserModal';
+
 
 const Authentication = () => {
-    const [open, setOpen] = useState(false)
+    const [open, setOpen] = useState(false);
+    const [users, setUsers] = useState([]);
+    const [searchResults, setSearchResults] = useState(users);
+
     const getHeader = () => {
-        const handleChange = (value) => {
-            console.log(value)
-        }
+        const handleSearch = (value) => {
+            filterData(value);
+        };
+
+        const filterData = (value) => {
+            const lowercasedValue = value.toLowerCase().trim();
+            if (lowercasedValue === '') setUsers(searchResults);
+            else {
+                const filteredData = searchResults.filter((item) => {
+                    return Object.keys(item).some((key) => 
+                    item[key].toString().toLowerCase().includes(lowercasedValue)
+                    );
+                });
+                setUsers(filteredData)
+            };
+        };
 
         const addUser = () => {
-            console.log('click')
-            setOpen(true)
-          
-        }
-
+            setOpen(true);
+        };
 
         return (
             <Box sx={cardHeaderStyles.wrapper}>
-                <SearchBar
-                    placeholder='Search by email, phone number, or user UID'
-                    onChange={(Event) => handleChange(Event.target.value)}
-                    searchBarWidth='520px'
+                <SearchBar 
+                    placeholder="Search by email address, phone number, or user UID"
+                    onChange={(event) => handleSearch(event.target.value)}
+                    searchBarWidth='720px'
                 />
                 <Box>
-                <CommonButton
-                    variant="contained"
-                    onClick={addUser}
-                    size="large"
-                    sx={cardHeaderStyles.addUserButton}
-                >
-                    Add user
-                </CommonButton>
-                <IconButton>
-                    <RefreshIcon />
-                </IconButton>
+                    <CommonButton 
+                        variant="contained"
+                        onClick={addUser}
+                        size="large"
+                        sx={cardHeaderStyles.addUserButton}
+                    >
+                        Add user
+                    </CommonButton>
+                    <IconButton>
+                        <RefreshIcon />
+                    </IconButton>
                 </Box>
             </Box>
         )
-    }
+    };
+
+    const addNewUser = (data) => {
+        users.push({ ...data });
+        setOpen(false);
+    };
 
     const getContent = () => (
-        <Typography
-            align="center"
-            sx={{ margin: '40px 16px', color: 'rgba(0, 0, 0, 0.6)', fontSize: '1.3rem' }}
-        >
-            No users for this project yet
-        </Typography>
+        <>
+            {
+                users.length ? 
+                    users.map((user) => (
+                        <Box sx={{ marginBottom: '20px' }} key={user.userId}>
+                            <Typography >User ID: {user.userId}</Typography>
+                            <Typography>Email: {user.email}</Typography>
+                            <Typography>Phone Number: {user.phoneNumber}</Typography>
+                        </Box>
+                    )) :
+                    <Typography 
+                        align="center"
+                        sx={{ margin: '40px 16px', color: 'rgba(0, 0, 0, 0.6)', fontSize: '1.3rem'}}
+                    >
+                        No users for this project yet
+                    </Typography>
+            }
+        </>
     );
 
-
     return (
-        <GripWrapper>
-
+        <GridWrapper>
             <BasicCard
                 header={getHeader()}
                 content={getContent()}
             />
-            <BasicModal open={open} onClose={() => setOpen(false)}/>
-        </GripWrapper>
+            <NewUserModal open={open} onClose={() => setOpen(false)} addNewUser={addNewUser}/>
+        </GridWrapper>
     )
 }
 
